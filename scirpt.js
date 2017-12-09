b0a7f2d1d4b9b5feaf6ed0c0fa11655e2f83cc03
SBarsCS <31691732+SBarsCS@users.noreply.github.com>
2017-12-09 21:26:43 +0400

    Update scirpt.js

--- scirpt.js
+++ scirpt.js
@@ -3,16 +3,35 @@ const context = canvas.getContext('2d');
 
 const score_field = document.getElementById('score');
 
+const enemy_one = new Image();
+enemy_one.src = 'images/enemy_1.png';
+
+const enemy_two = new Image();
+enemy_two.src = 'images/enemy_2.png';
+
+const background = new Image();
+background.src = 'images/background.jpg';
+
 var jumping = false;
 
 const penguin = {
     x: 100,
-    y: 670,
+    y: 570,
     height: 100,
     width: 50,
     //penguins jumping force
     force: 20,
-    score: 0
+    score: 0,
+    change: 0
+};
+
+const background_loop = {
+    one: {
+        x: 0
+    },
+    two: {
+        x: canvas.width
+    }
 };
 
 score_field.innerHTML = penguin.score;
@@ -20,151 +39,152 @@ score_field.innerHTML = penguin.score;
 const enemy = {
     enemy1: {
         x: canvas.width,
-        y: 670,
-        height: 100,
-        width: 50,
+        y: 597,
+        height: 75,
+        width: 150,
         done: 0
     },
     enemy2: {
-        x: canvas.width + 500,
-        y: 670,
-        height: 100,
-        width: 50,
-        done: 0
-    },
-    enemy3: {
-        x: canvas.width + 1000,
-        y: 670,
-        height: 100,
-        width: 50,
+        x: canvas.width + 750,
+        y: 554,
+        height: 125,
+        width: 95,
         done: 0
     }
 };
 
-function drawer(){
+function drawer() {
     context.clearRect(0,0,canvas.width, canvas.height);
-    context.fillStyle = 'green';
-    context.fillRect(0, 770, canvas.width, 30);
-    context.fillStyle = 'black';
+
+    //drawing background
+    context.drawImage(background, background_loop.one.x, 0, canvas.width, canvas.height);
+    context.drawImage(background, background_loop.two.x, 0, canvas.width, canvas.height);
+
+    //drawing hero
     context.fillRect(penguin.x, penguin.y, penguin.width, penguin.height);
-    context.fillStyle = 'red';
-    context.fillRect(enemy.enemy1.x, enemy.enemy1.y, enemy.enemy1.width, enemy.enemy1.height);
-    context.fillRect(enemy.enemy2.x, enemy.enemy2.y, enemy.enemy2.width, enemy.enemy2.height);
-    context.fillRect(enemy.enemy3.x, enemy.enemy3.y, enemy.enemy3.width, enemy.enemy3.height);
-}
 
-function updater(){
-    enemy.enemy1.x -= 5;
-    enemy.enemy2.x -= 5;
-    enemy.enemy3.x -= 5;
-
-    if(enemy.enemy1.x <= -300){
-        enemy.enemy1.x = canvas.width;
-    }
-
-    if(enemy.enemy2.x <= -300){
-        enemy.enemy2.x = canvas.width;
-    }
-
-    if(enemy.enemy3.x <= -300){
-        enemy.enemy3.x = canvas.width;
-    }
+    //drawing enemies
+    context.drawImage(enemy_one, enemy.enemy1.x, enemy.enemy1.y, enemy.enemy1.width, enemy.enemy1.height);
+    context.drawImage(enemy_two, enemy.enemy2.x, enemy.enemy2.y, enemy.enemy2.width, enemy.enemy2.height)
 
 }
 
-function loop(){
-    drawer();
-    jump_engine();
-    updater();
-    collision_checker();
-    score_checker();
+    function updater() {
+        enemy.enemy1.x -= 5;
+        enemy.enemy2.x -= 5;
 
-    requestAnimationFrame(loop);
-}
+        background_loop.one.x -= 5;
 
-loop();
-
-document.addEventListener('keydown', function(event) {
-    if(event.keyCode === 32){
-        if(penguin.y === 670){
-            jumping = true;
+        if(background_loop.one.x <= -canvas.width){
+            background_loop.one.x = canvas.width;
         }
 
-    }
-}, false);
+        background_loop.two.x -= 5;
 
-function jump_engine(){
-    if(jumping === true){
-        penguin.y -= penguin.force;
-        //speed of falling
-        penguin.force -= 0.7;
-        if(penguin.y >= 670){
-            jumping = false;
-            //resetting penguins jumping force
-            penguin.force = 20;
-            penguin.y = 670;
+        if(background_loop.two.x <= -canvas.width){
+            background_loop.two.x = canvas.width;
         }
-    }
-}
 
-function collision_checker() {
-    //Checking collision with enemy 1
-    if(penguin.x >= enemy.enemy1.x - 40 && penguin.x <= enemy.enemy1.x + 40){
-        if(penguin.y >= 550 - (penguin.force - 20)){
-            alert("You lost");
+        if (enemy.enemy1.x <= -enemy.enemy1.width) {
+            enemy.enemy1.x = canvas.width;
         }
-    }
 
-    //Checking collision with enemy 2
-    if(penguin.x >= enemy.enemy2.x - 40 && penguin.x <= enemy.enemy2.x + 40){
-        if(penguin.y >= 550 - (penguin.force - 20)){
-            alert("You lost");
+        if (enemy.enemy2.x <= -enemy.enemy1.width) {
+            enemy.enemy2.x = canvas.width;
         }
-    }
 
-    //Checking collision with enemy 3
-    if(penguin.x >= enemy.enemy3.x - 40 && penguin.x <= enemy.enemy3.x + 40){
-        if(penguin.y >= 550 - (penguin.force - 20)){
-            alert("You lost");
-        }
-    }
-}
-
-
-function score_checker() {
-    if(penguin.x >= enemy.enemy1.x - 40 && penguin.x <= enemy.enemy1.x + 40){
-        if(penguin.y <= 550 - (penguin.force - 20)){
-            if(enemy.enemy1.done === 0) {
-                penguin.score++;
-                score_field.innerHTML = penguin.score;
-                enemy.enemy1.done = 1;
+        if(penguin.score % 10 === 0 && penguin.score !== 0 && penguin.change !== penguin.score){
+            if(enemy.enemy1.width === 150){
+                background.src = 'images/background_2.jpg';
+                enemy.enemy1.width = 75;
+                enemy.enemy1.height = 125;
+                enemy_one.src = 'images/enemy_3.png';
+                enemy_two.src = 'images/enemy_4.png';
+            }else{
+                background.src = 'images/background.jpg';
+                enemy.enemy1.width = 150;
+                enemy.enemy1.height = 100;
+                enemy_one.src = 'images/enemy_1.png';
+                enemy_two.src = 'images/enemy_2.png';
             }
-            enemy.enemy3.done = 0;
-            enemy.enemy2.done = 0;
+            penguin.change = penguin.score;
+        }
+
+    }
+
+    function loop() {
+        drawer();
+        jump_engine();
+        updater();
+        collision_checker();
+        score_checker();
+
+        requestAnimationFrame(loop);
+    }
+
+    loop();
+
+    document.addEventListener('keydown', function (event) {
+        if (event.keyCode === 32) {
+            if (penguin.y === 570) {
+                jumping = true;
+            }
+
+        }
+    }, false);
+
+    function jump_engine() {
+        if (jumping === true) {
+            penguin.y -= penguin.force;
+            //speed of falling
+            penguin.force -= 0.7;
+            if (penguin.y >= 570) {
+                jumping = false;
+                //resetting penguins jumping force
+                penguin.force = 20;
+                penguin.y = 570;
+            }
         }
     }
 
-    if(penguin.x >= enemy.enemy2.x - 40 && penguin.x <= enemy.enemy2.x + 40){
-        if(penguin.y <= 550 - (penguin.force - 20)){
-            if(enemy.enemy2.done === 0) {
-                penguin.score++;
-                score_field.innerHTML = penguin.score;
-                enemy.enemy2.done = 1;
+    function collision_checker() {
+        //Checking collision with enemy 1
+        if (penguin.x >= enemy.enemy1.x - 40 && penguin.x <= enemy.enemy1.x + enemy.enemy1.width - 10) {
+            if (penguin.y >= 475 - (penguin.force - 25)) {
+                alert("You lost");
             }
-            enemy.enemy1.done = 0;
-            enemy.enemy3.done = 0;
         }
+
+        //Checking collision with enemy 2
+        if (penguin.x >= enemy.enemy2.x - 40 && penguin.x <= enemy.enemy2.x + enemy.enemy2.width - 10) {
+            if (penguin.y >= 425 - (penguin.force - 20)) {
+                alert("You lost");
+            }
+        }
+
     }
 
-    if(penguin.x >= enemy.enemy3.x - 40 && penguin.x <= enemy.enemy3.x + 40){
-        if(penguin.y <= 550 - (penguin.force - 20)){
-            if(enemy.enemy3.done === 0) {
-                penguin.score++;
-                score_field.innerHTML = penguin.score;
-                enemy.enemy3.done = 1;
+
+    function score_checker() {
+        if (penguin.x >= enemy.enemy1.x - 40 && penguin.x <= enemy.enemy1.x + enemy.enemy1.width - 10) {
+            if (penguin.y <= 475 - (penguin.force - 20)) {
+                if (enemy.enemy1.done === 0) {
+                    penguin.score++;
+                    score_field.innerHTML = penguin.score;
+                    enemy.enemy1.done = 1;
+                }
+                enemy.enemy2.done = 0;
             }
-            enemy.enemy2.done = 0;
-            enemy.enemy1.done = 0;
+        }
+
+        if (penguin.x >= enemy.enemy2.x - 40 && penguin.x <= enemy.enemy2.x + enemy.enemy2.width - 10) {
+            if (penguin.y <= 425 - (penguin.force - 20)) {
+                if (enemy.enemy2.done === 0) {
+                    penguin.score++;
+                    score_field.innerHTML = penguin.score;
+                    enemy.enemy2.done = 1;
+                }
+                enemy.enemy1.done = 0;
+            }
         }
     }
-}
